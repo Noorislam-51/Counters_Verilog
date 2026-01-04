def97e83eb7899e698ef9eb0843783b3018b2434
Noor Islam <noorislam5114@gmail.com>
2026-01-04 09:15:48 +0530

    Update up_counter.v

--- UP_COUNTER/up_counter.v
+++ UP_COUNTER/up_counter.v
@@ -47,17 +47,43 @@ dff dff3 (.q(q[3]),.d(d3),.clk(clk),.rst(rst));
 
 endmodule
 
-// ASYNC UP-COUNTER_BEHAVIORAL--------------------------
-module up_counter_async_bh(q,clk,rst);
-input clk,rst;
-output reg [3:0]q;
+// ASYNC UP-COUNTER_BEHAVIORAL--------------------
+module async_counter (
+    input  wire clk,
+    input  wire rst,
+    output reg  [3:0] q
+);
 
-always@(negedge clk or posedge rst)begin
-  if(rst)
-    q<=1'b0;
-  else
-    q<=q+1;
-end
+    // LSB toggles with main clock
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            q[0] <= 1'b0;
+        else
+            q[0] <= ~q[0];
+    end
+
+    // Next bit toggles with previous bit
+    always @(posedge q[0] or posedge rst) begin
+        if (rst)
+            q[1] <= 1'b0;
+        else
+            q[1] <= ~q[1];
+    end
+
+    always @(posedge q[1] or posedge rst) begin
+        if (rst)
+            q[2] <= 1'b0;
+        else
+            q[2] <= ~q[2];
+    end
+
+    always @(posedge q[2] or posedge rst) begin
+        if (rst)
+            q[3] <= 1'b0;
+        else
+            q[3] <= ~q[3];
+    end
+
 endmodule
 
 // SYNC UP-COUNTER_BEHAVIORAL--------------------------
